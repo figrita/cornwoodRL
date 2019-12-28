d476271d30f42d81296ede834a3e27815035affd
figrita <josephdavidruiz@gmail.com>
2019-12-27 22:08:58 -0500

    Changed Code to PyGame, Assets to DawnLike

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # cornwoodRL
 A python3 Roguelike for fun
 
-Depends on TCOD.  [Installation instructions Here](https://python-tcod.readthedocs.io/en/latest/installation.html)
+Depends on pygame. [Check Out Here](https://www.pygame.org/wiki/GettingStarted) to get install instructions.
+
+Tileset is [DawnLike by DragonDePlatino](https://opengameart.org/content/dawnlike-16x16-universal-rogue-like-tileset-v181). It uses Dawnbringer's 16-color palette.
+
+To run the program, from inside this folder execute ```python3 engine.py```
---
+++ engine.py
@@ -0,0 +1,169 @@
+import random
+import os
+
+# import basic pygame modules
+import pygame as pg
+
+import spritesheet
+
+# see if we can load more than standard BMP
+if not pg.image.get_extended():
+    raise SystemExit("Sorry, extended image module required")
+
+
+# game constants
+SCREENRECT = pg.Rect(0, 0, 640, 480)
+SCORE = 0
+
+main_dir = os.path.split(os.path.abspath(__file__))[0]
+
+
+# Each type of game object gets an init and an update function.
+# The update function is called once per frame, and it is when each object should
+# change it's current position and state.
+#
+# The Player object actually gets a "move" function instead of update,
+# since it is passed extra information about the keyboard.
+
+
+class Player(pg.sprite.Sprite):
+    """ Representing the player as a moon buggy type car.
+    """
+
+    speed = 10
+    bounce = 24
+    gun_offset = -11
+    images = []
+
+    def __init__(self):
+        pg.sprite.Sprite.__init__(self, self.containers)
+        self.image = self.images[0]
+        self.rect = self.image.get_rect(midbottom=SCREENRECT.midbottom)
+        self.reloading = 0
+        self.origtop = self.rect.top
+        self.facing = -1
+
+    def move(self, direction):
+        if direction:
+            self.facing = direction
+        self.rect.move_ip(direction * self.speed, 0)
+        self.rect = self.rect.clamp(SCREENRECT)
+        if direction < 0:
+            self.image = self.images[0]
+        elif direction > 0:
+            self.image = self.images[1]
+        self.rect.top = self.origtop - (self.rect.left // self.bounce % 2)
+
+    def gunpos(self):
+        pos = self.facing * self.gun_offset + self.rect.centerx
+        return pos, self.rect.top
+
+    def update(self):
+        """ We only update the score in update() when it has changed.
+        """
+        return 0
+
+
+def main(winstyle=0):
+    # Initialize pygame
+    if pg.get_sdl_version()[0] == 2:
+        pg.mixer.pre_init(44100, 32, 2, 1024)
+    pg.init()
+    if pg.mixer and not pg.mixer.get_init():
+        print("Warning, no sound")
+        pg.mixer = None
+    pg.mixer = None
+
+    fullscreen = False
+    # Set the display mode
+    winstyle = 0  # |FULLSCREEN
+    bestdepth = pg.display.mode_ok(SCREENRECT.size, winstyle, 32)
+    screen = pg.display.set_mode(SCREENRECT.size, winstyle, bestdepth)
+
+    # Load images, assign to sprite classes
+    # (do this before the classes are used, after screen setup)
+    ss = spritesheet.spritesheet('DawnLike/Characters/Cat0.png')
+    img = ss.image_at((0, 0, 16, 16))
+    Player.images = [img, pg.transform.flip(img, 1, 0)]
+
+    # decorate the game window
+    icon = pg.transform.scale(img, (32, 32))
+    pg.display.set_icon(icon)
+    pg.display.set_caption("Pygame Aliens")
+    pg.mouse.set_visible(0)
+
+    # create the background, tile the bgd image
+    bs = spritesheet.spritesheet('DawnLike/Objects/Tile.png')
+    bgdtile = bs.image_at((0, 0, 16, 16))
+    background = pg.Surface(SCREENRECT.size)
+    for x in range(0, SCREENRECT.width, bgdtile.get_width()):
+        background.blit(bgdtile, (x, 0))
+    screen.blit(background, (0, 0))
+    pg.display.flip()
+
+    all = pg.sprite.RenderUpdates()
+
+    # assign default groups to each sprite class
+    Player.containers = all
+
+    # Create Some Starting Values
+    clock = pg.time.Clock()
+    player = Player()
+
+    # Run our main loop whilst the player is alive.
+    while player.alive():
+
+        # get input
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
+            if event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
+                return
+            elif event.type == pg.KEYDOWN:
+                if event.key == pg.K_f:
+                    if not fullscreen:
+                        print("Changing to FULLSCREEN")
+                        screen_backup = screen.copy()
+                        screen = pg.display.set_mode(
+                            SCREENRECT.size, winstyle | pg.FULLSCREEN, bestdepth
+                        )
+                        screen.blit(screen_backup, (0, 0))
+                    else:
+                        print("Changing to windowed mode")
+                        screen_backup = screen.copy()
+                        screen = pg.display.set_mode(
+                            SCREENRECT.size, winstyle, bestdepth
+                        )
+                        screen.blit(screen_backup, (0, 0))
+                    pg.display.flip()
+                    fullscreen = not fullscreen
+
+        keystate = pg.key.get_pressed()
+
+        # clear/erase the last drawn sprites
+        all.clear(screen, background)
+
+        # update all the sprites
+        all.update()
+
+        # handle player input
+        direction = keystate[pg.K_RIGHT] - keystate[pg.K_LEFT]
+        player.move(direction)
+        
+
+        # draw the scene
+        dirty = all.draw(screen)
+        pg.display.update(dirty)
+
+        # cap the framerate at 40fps. Also called 40HZ or 40 times per second.
+        clock.tick(40)
+
+    if pg.mixer:
+        pg.mixer.music.fadeout(1000)
+    pg.time.wait(1000)
+    pg.quit()
+
+
+# call the "main" function if running this script
+if __name__ == "__main__":
+    main()
